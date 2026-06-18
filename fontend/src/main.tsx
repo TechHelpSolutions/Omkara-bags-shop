1ce7a30d27edebf07dfbea6b56c317dfbb7e5191
TechHelpSolutions <saurabhmaurya6746@gmail.com>
2026-06-18 10:41:24 +0530

    Update main.tsx

--- fontend/src/main.tsx
+++ fontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles.css"; // Agar tumhara CSS global path alag hai to adjust kar lena
+import "styles.css"; // Agar tumhara CSS global path alag hai to adjust kar lena
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
