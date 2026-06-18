82448dfaf3aa0bc9f6586b9eea288f994f261dba
TechHelpSolutions <saurabhmaurya6746@gmail.com>
2026-06-18 10:50:41 +0530

    Update vite.config.ts

--- fontend/vite.config.ts
+++ fontend/vite.config.ts
@@ -6,11 +6,14 @@
 // You can pass additional config via defineConfig({ vite: { ... }, etc... }) if needed.
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     react(),
-    tsconfigPaths()
+    tailwindcss(),
   ],
+  resolve: {
+    tsconfigPaths: true,
+  },
 });
